a7bbdc6be0b6e207ef4afde5f3d16e9f95130644
BessDaniil <102468401+BessDaniil@users.noreply.github.com>
2022-08-17 15:53:42 +0400

    Update PizzeriasDto.java

--- src/main/java/com/test/dodo/model/PizzeriasDto.java
+++ src/main/java/com/test/dodo/model/PizzeriasDto.java
@@ -1,7 +1,8 @@
 package com.test.dodo.model;
 
 import java.util.List;
-
+@Getter
+@ToString
 public class PizzeriasDto {
     private List<PizzeriaDto> countries;
     private List<PizzeriaDto> errors;
